c2b9b53dea46d7fd48c385f0af8e9c996e1f4594
Emily <emily.clyde@yahoo.com>
2015-02-19 13:00:49 -0800

    Test working
    Tests MrMoney

--- test1/AccountTest.cs
+++ test1/AccountTest.cs
@@ -10,79 +10,87 @@ namespace ATM_Application.UnitTests
   [TestFixture]
   class AccountTest
   {
-Account myMoney = new Account() { Balance = 1000, AccountNumber=12345 };
+    Account myMoney = new Account() { Balance = 1000, AccountNumber = 12345 };
 
-//***********************************************************
-// Check Account Number
-//***********************************************************
-[Test]
-public void CheckAccountNum()
-{
-    int x = myMoney.ShowAccountNum();
+    //***********************************************************
+    // Check Account Number
+    //***********************************************************
+    [Test]
+    public void CheckAccountNum()
+    {
+      int x = myMoney.ShowAccountNum();
 
-    Assert.AreEqual(x, 12345);
-}
+      Assert.AreEqual(x, 12345);
+    }
 
 
-//***********************************************************
-// Check Balance
-//***********************************************************
-    [Test]   
+    //***********************************************************
+    // Check Balance
+    //***********************************************************
+    [Test]
     public void CheckBalance()
     {
-        decimal x = myMoney.ShowBalance();
+      decimal x = myMoney.ShowBalance();
 
       Assert.AreEqual(x, 1000);
     }
 
 
-//***********************************************************
-// Deposit
-//***********************************************************
+    //***********************************************************
+    // Deposit
+    //***********************************************************
     [Test]   //input a positive ammont; Balance changes
     public void DepositFunds()
     {
-        myMoney.AddToBalance(100);
-       
-      Assert.AreEqual(1100, myMoney);
+      myMoney.Balance = 1000;
+      myMoney.AddToBalance(100);
+
+      //decimal foo = myMoney.Balance;
+
+      Assert.AreEqual(myMoney.Balance, (decimal)1100);
     }
 
     [Test]   //input a negative ammont; no change to Balance
-      public void DepositFundsNeg()
-      {
-          myMoney.AddToBalance(-100);
+    public void DepositFundsNeg()
+    {
 
-          Assert.AreEqual(1000, myMoney);
-      }
+      myMoney.Balance = 1000;
+      myMoney.AddToBalance(-100);
 
-//***********************************************************
-// Withdrawl
-//***********************************************************
+      Assert.AreEqual(myMoney.Balance, (decimal)1000);
+    }
+
+    //***********************************************************
+    // Withdrawl
+    //***********************************************************
     [Test] //input a positive ammont with sufficient funds; Balance changes
     public void WithdrawFunds()
     {
-        myMoney.SubFromBalance(100);
+      myMoney.Balance = 1000;
+      myMoney.SubFromBalance(100);
 
-      Assert.AreNotEqual(900, myMoney);
+      Assert.AreEqual((decimal)900, myMoney.Balance);
     }
 
     [Test] //input a positive ammont with insufficient funds; no change to Balance 
     public void WithdrawFundsInsuffFunds()
     {
-        myMoney.SubFromBalance(1200);
+      myMoney.Balance = 1000;
+      myMoney.SubFromBalance(1200);
 
-        Assert.AreEqual(1000, myMoney);
+      Assert.AreEqual((decimal)1000, myMoney.Balance);
     }
 
     [Test] //input a negative ammont; no change to Balance 
     public void WithdrawFundsNeg()
     {
-        myMoney.SubFromBalance(-100);
+      myMoney.Balance = 1000;
+      myMoney.SubFromBalance(-100);
 
-        Assert.AreEqual(1000, myMoney);
+      Assert.AreEqual((decimal)1000, myMoney.Balance);
     }
 
-      
+
   }
 }
 
